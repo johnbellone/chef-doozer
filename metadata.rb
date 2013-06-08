8a5775bc4d536100f4255e88e6e99018ec7e5d4e
John Bellone <john.bellone.jr@gmail.com>
2013-06-08 11:49:43 -0400

    Bump 'metadata.rb' version.
    Also adds some other information about the cookbook.

--- metadata.rb
+++ metadata.rb
@@ -3,8 +3,10 @@ maintainer              "John Bellone"
 maintainer_email        "john.bellone.jr@gmail.com"
 license                 "Apache 2.0"
 description             "Manages doozer and its installed libraries."
-#long_description        IO.read('README.md')
-version                 "0.0.1"
+long_description        IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version                 "0.1.0"
+recipe                  "doozer", "Main Doozer configuration"
+recipe                  "doozer::doozer", "Doozer CLI"
 
 depends 'build-essential'
 depends 'golang'
