43e505db79adc37efd21476df97743c893efe534
John Bellone <john.bellone.jr@gmail.com>
2013-06-27 23:07:47 -0400

    Add iptables as suggests and bump version.

--- metadata.rb
+++ metadata.rb
@@ -4,13 +4,16 @@ maintainer_email        "john.bellone.jr@gmail.com"
 license                 "Apache 2.0"
 description             "Manages doozer and its installed libraries."
 long_description        IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version                 "0.1.0"
+version                 "0.2.0"
 recipe                  "doozer", "Main Doozer configuration"
 recipe                  "doozer::doozer", "Doozer CLI"
+recipe                  "doozer::doozerd", "Doozer server"
 
 depends 'build-essential'
 depends 'golang'
 
 supports 'redhat', '>= 6.0'
 supports 'centos', '>= 6.0'
-supports 'ubuntu'
+supports 'ubuntu', '>= 6.10'
+
+suggests 'iptables'
